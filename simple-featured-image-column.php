a6d779471e1e0c856a2efc5d9c23b801fde715cc
André Villela <dede.villela@gmail.com>
2017-12-19 12:45:26 -0200

    Patched braces, spacing and indentation

--- simple-featured-image-column.php
+++ simple-featured-image-column.php
@@ -3,17 +3,17 @@
  * Plugin Name: Simple Featured Image Column
  * Plugin URI: https://github.com/dedevillela/Simple-Featured-Image-Column/
  * Description: A simple plugin that displays the "Featured Image" column in admin post type listing. Supports Post, Pages and Custom Posts.
- * Version: 1.0.0
+ * Version: 1.0.7
  * Author: Andre Aguiar Villela
  * Author URI: https://dedevillela.com/
  * License: GPLv2+
  **/
 
-  if ( !defined( 'ABSPATH' ) || preg_match('#' . basename( __FILE__ ) . '#',  $_SERVER['PHP_SELF'])) {
-    die( "Hey, dude! What are you doing here?" );
+  if (!defined( 'ABSPATH' ) || preg_match('#'.basename( __FILE__ ).'#', $_SERVER['PHP_SELF'])) {
+  	die("Hey, dude! What are you doing here?");
   }
 
-  if ( !class_exists( 'Simple_Featured_Image_Column' ) ) {
+  if (!class_exists('Simple_Featured_Image_Column')) {
 
 	class Simple_Featured_Image_Column {
 
@@ -21,28 +21,36 @@
 			add_action('admin_init', array($this, 'init'));
 		}
 
-		function init(){
+		function init() {
 
 			$post_types = apply_filters('Simple_Featured_Image_Column_post_types', get_post_types(array('public' => true)));
-			if(empty($post_types)) return;
+			if (empty($post_types)) {
+				return;
+			}
 
-			add_action('admin_head', function(){ 
+			add_action('admin_head', function() { 
 				echo '<style>th#featured-image  { width: 100px; }</style>'."\r\n"; 
 			});
 			
-			foreach($post_types as $post_type){
-				if(!post_type_supports($post_type, 'thumbnail')) continue;
-				add_filter( "manage_{$post_type}_posts_columns", array($this, 'columns'));
-				add_action( "manage_{$post_type}_posts_custom_column", array($this, 'column_data'), 10, 2);
+			foreach ($post_types as $post_type) {
+				if (!post_type_supports($post_type, 'thumbnail')) {
+					continue;
+				}
+				add_filter("manage_{$post_type}_posts_columns", array($this, 'columns'));
+				add_action("manage_{$post_type}_posts_custom_column", array($this, 'column_data'), 10, 2);
 			}
 		}
 
-		function columns($columns){
+		function columns($columns) {
 			
-			if(!is_array($columns)) $columns = array();
+			if (!is_array($columns)) {
+				$columns = array();
+			}
 			$new = array();
 			foreach($columns as $key => $title){
-				if($key == 'title') $new['featured-image'] = __('Image', 'wordpress');
+				if ($key == 'title') {
+					$new['featured-image'] = __('Image', 'wordpress');
+				}
 				$new[$key] = $title;
 			}
 			return $new;
@@ -50,19 +58,19 @@
 
 		function column_data($column_name, $post_id) {
 			
-			if('featured-image' != $column_name) return;
+			if ('featured-image' != $column_name) {
+				return;
+			}
 			$style = 'display: block; max-width: 100px; height: auto; border: 1px solid #e5e5e5;';
 			$style = apply_filters('Simple_Featured_Image_Column_image_style', $style);
 
-			if(has_post_thumbnail($post_id)){
+			if (has_post_thumbnail($post_id)) {
 				$size = 'thumbnail';
 				echo get_the_post_thumbnail($post_id, $size, 'style='.$style);
 			} else {
-				echo '<img style="'. $style .'" src="'. esc_url(plugins_url( 'images/default.png', __FILE__ )) .'" />';
+				echo '<img style="'. $style .'" src="'. esc_url(plugins_url('images/default.png', __FILE__)) .'" />';
 			}	
 		}
 	}
-
 	$featured_image_column = new Simple_Featured_Image_Column;
-
 };
