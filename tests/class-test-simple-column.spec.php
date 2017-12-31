2d0b95076ac020ee15302553660e0e83700034a0
André Villela <dede.villela@gmail.com>
2017-12-31 17:23:34 -0200

    Added $plugin_name and $version strings

---
+++ tests/class-test-simple-column.spec.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class Test_Simple_Column.
+ *
+ * @package Simple_Featured_Image_Column
+ */
+
+/**
+ * Class that tests the simple featured image column
+ */
+class Test_Simple_Column extends WP_UnitTestCase {
+
+/**
+ * The ID of this plugin.
+ *
+ * @since    1.0.8
+ * @access   private
+ * @var      string    $plugin_name    The ID of this plugin.
+ */
+  private $plugin_name = 'simple-featured-image-column';
+	
+/**
+ * The version of this plugin.
+ *
+ * @since    1.0.0
+ * @access   private
+ * @var      string    $version    The current version of this plugin.
+ */
+  private $version = '1.0.8';
+}
