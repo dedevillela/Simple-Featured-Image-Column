b9b6d7484fd39c8ccc9eae2135291b0d45714ced
André Villela <dede.villela@gmail.com>
2018-01-03 02:19:55 -0200

    Update class-test-simple-column.spec.php

--- tests/class-test-simple-column.spec.php
+++ tests/class-test-simple-column.spec.php
@@ -5,8 +5,6 @@
  * @package Simple_Featured_Image_Column
  */
 
-use Simple_Featured_Image_Column;
-
 /**
  * Class that tests the simple featured image column
  */
@@ -19,6 +17,7 @@ class Test_Simple_Column extends WP_UnitTestCase {
  * @access   private
  * @var      string    $plugin_name    The ID of this plugin.
  */
+	
 	private $plugin_name = 'simple-featured-image-column';
 	
 /**
@@ -33,17 +32,6 @@ class Test_Simple_Column extends WP_UnitTestCase {
 	function setUp() {
 		parent::setUp();
 		$this->sfic = new Simple_Featured_Image_Column;
+		$this->assertTrue( true );
 	}
-	
-/**
- * Test the columns method
- *
- * Test if the method columns exists.
- *
- * @since 1.0.8
- */
-  public function test_method_type() {
-    $columns = new Simple_Featured_Image_Column( $this->plugin_name, $this->version );
-    $this->assertTrue( method_exists( $columns , 'columns' ) );
-  }
 }
