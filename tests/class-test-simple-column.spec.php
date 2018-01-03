516706cc3345911d2d4134479502644559272392
André Villela <dede.villela@gmail.com>
2018-01-03 01:39:58 -0200

    Added test for the column_data method

--- tests/class-test-simple-column.spec.php
+++ tests/class-test-simple-column.spec.php
@@ -5,6 +5,8 @@
  * @package Simple_Featured_Image_Column
  */
 
+use Simple_Featured_Image_Column;
+
 /**
  * Class that tests the simple featured image column
  */
@@ -22,7 +24,7 @@ class Test_Simple_Column extends WP_UnitTestCase {
 /**
  * The version of this plugin.
  *
- * @since    1.0.0
+ * @since    1.0.8
  * @access   private
  * @var      string    $version    The current version of this plugin.
  */
@@ -42,4 +44,16 @@ class Test_Simple_Column extends WP_UnitTestCase {
 		);
 		return wp_insert_post( array_merge( $defaults, $post ) );
 	}
+	
+/**
+ * Test the column_data method
+ *
+ * Test if the method column_data exists.
+ *
+ * @since 1.0.8
+ */
+  public function test_method_type() {
+    $featured_image_column = new Simple_Featured_Image_Column( $this->plugin_name, $this->version );
+    $this->assertTrue( method_exists( $featured_image_column , 'column_data' ) );
+  }
 }
