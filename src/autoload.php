90c4641b01f99f151c2e523013fe028e041cf1c7
André Villela <dede.villela@gmail.com>
2019-08-21 00:44:25 -0300

    removed extra spacings

--- src/autoload.php
+++ src/autoload.php
@@ -6,16 +6,16 @@
  * Code inspired from the SplClassLoader RFC
  * @see https://wiki.php.net/rfc/splclassloader#example_implementation
  */
-spl_autoload_register(function ($className) {
+spl_autoload_register(function($className) {
 	$className = ltrim($className, '\\');
 	$fileName = '';
 	$namespace = '';
 	if ($lastNsPos = strripos($className, '\\')) {
 		$namespace = substr($className, 0, $lastNsPos);
 		$className = substr($className, $lastNsPos + 1);
-		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace).DIRECTORY_SEPARATOR;
 	}
-	$fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . $className . '.php';
+	$fileName = __DIR__.DIRECTORY_SEPARATOR.$fileName.$className.'.php';
 	if (file_exists($fileName)) {
 		require $fileName;
 		return true;
