16793f92c17e288d974dbf199f526d5ddf720b16
André Villela <dede.villela@gmail.com>
2019-08-21 00:14:35 -0300

    Removed spacing between variables

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,21 +5,21 @@
  * @package Simple_Featured_Image_Column
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
+$_tests_dir = getenv('WP_TESTS_DIR');
+if (!$_tests_dir) {
   $_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+require_once $_tests_dir.'/includes/functions.php';
 
 /**
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-  require dirname( __DIR__ ) . '/src/class-simple-featured-image-column.php';
+  require dirname(__DIR__).'/src/class-simple-featured-image-column.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require $_tests_dir.'/includes/bootstrap.php';
