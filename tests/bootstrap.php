75b4d1ac8ed2ec652977dcfbf72f56fac5f21f10
André Villela <dede.villela@gmail.com>
2019-08-21 01:14:38 -0300

    Added conditional to load functions.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,11 @@
 
 $_tests_dir = getenv('WP_TESTS_DIR');
 if (!$_tests_dir) {
-  $_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+if (!file_exists($_tests_dir.'/includes/functions.php')) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
+	exit(1);
 }
 
 // Give access to tests_add_filter() function.
@@ -17,8 +21,9 @@ require_once $_tests_dir.'/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-  require dirname(__DIR__).'/src/class-simple-featured-image-column.php';
+	require dirname(__DIR__).'/src/class-simple-featured-image-column.php';
 }
+/** @scrutinizer ignore-call */
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
 // Start up the WP testing environment.
